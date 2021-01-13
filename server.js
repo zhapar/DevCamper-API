b386f8bd024d8b26c7cb57ef796404bf73489f0f
Zhapar Manas uulu <zhapar28@gmail.com>
2021-01-14 00:26:41 +0600

    Created CRUD functionality for Bootcamps

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/error');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close the server and exit process
+  server.close(() => process.exit(1));
+});
